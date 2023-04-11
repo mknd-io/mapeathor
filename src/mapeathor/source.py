370c684255b6b09f2e9c98c470a179d33b1b6529
Denis Joubert <denis@thotsecurity.ch>
2023-04-11 23:53:36 +0200

    don't generate yarrml if not modified

--- src/mapeathor/source.py
+++ src/mapeathor/source.py
@@ -1,8 +1,12 @@
+import os
 import pandas
 import tempfile
 import re
 import sys
 import requests
+import hashlib
+import shutil
+from pathlib import Path
 
 
 def checkFile(path):
@@ -16,9 +20,30 @@ def checkFile(path):
         return False
 
 
-def gdriveToXMLX(url):
-    temp = tempfile.NamedTemporaryFile(prefix="mapeathor-gdrive", delete=False, suffix=".xlsx")
 
+def hash_file(filename):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
+
+   # make a hash object
+   h = hashlib.sha1()
+
+   # open file for reading in binary mode
+   with open(filename,'rb') as file:
+
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   # return the hex representation of digest
+   return h.hexdigest()
+
+def gdriveToXMLX(url, path):
+    temp = tempfile.NamedTemporaryFile(prefix="mapeathor-gdrive", delete=False, suffix=".xlsx") 
+    localfile = path+'.xlsx'
     m = re.search(r'(?:file|spreadsheets)\/d\/(.*)\/', url)
 
     if not m:
@@ -30,5 +55,14 @@ def gdriveToXMLX(url):
     url = 'https://docs.google.com/spreadsheets/d/'+docid+'/export?exportFormat=xlsx'
 
     r = requests.get(url)
+
     temp.write(r.content)
-    return temp.name
+    temp.close()
+    if not (Path(localfile).is_file()) or hash_file(temp.name) != hash_file(localfile):
+        shutil.copyfile(temp.name, localfile)
+        if Path(path+'.yaml').is_file():
+            os.remove(path+'.yaml')
+        return localfile
+    else:
+        return ''
+
